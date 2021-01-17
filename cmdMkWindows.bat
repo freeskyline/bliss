f99ad96da1866373334af25d5577b2cb5d110587
LIHUI <110496364@qq.com>
2021-01-17 11:07:11 +0800

    Initial working Web Server.

---
+++ cmdMkWindows.bat
@@ -0,0 +1,2 @@
+go build webServer.go
+pause
---
+++ webServer.go
@@ -0,0 +1,18 @@
+//webServer.go, A Sample Web Server
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
